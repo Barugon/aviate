4db3dd0eb458b1bcdafd3c2b62fc34b53dead5fe
Barugon <16503728+Barugon@users.noreply.github.com>
2022-08-03 07:15:17 -0700

    Simplify SelectDlg

--- src/select_dlg.rs
+++ src/select_dlg.rs
@@ -1,11 +1,10 @@
 use eframe::{egui, emath};
-use std::path::PathBuf;
 
 #[derive(Default)]
 pub struct SelectDlg;
 
 impl SelectDlg {
-  pub fn show(&mut self, ctx: &egui::Context, choices: Choices) -> Option<Response> {
+  pub fn show(&mut self, ctx: &egui::Context, choices: Vec<String>) -> Option<Response> {
     let mut selection = None;
     let mut open = true;
     egui::Window::new(egui::RichText::from("🌐  Select").strong())
@@ -16,35 +15,16 @@ impl SelectDlg {
       .default_width(200.0)
       .show(ctx, |ui| {
         ui.add_space(8.0);
-        ui.vertical_centered(|ui| match &choices {
-          Choices::Paths(paths) => {
-            for (index, path) in paths.iter().enumerate() {
-              ui.horizontal(|ui| {
-                let text = path.file_stem().unwrap().to_str().unwrap();
-                let button = egui::Button::new(text);
-                if ui.add_sized(ui.available_size(), button).clicked() {
-                  selection = Some(Response::Index(index));
-                }
-              });
-            }
-          }
-          Choices::Strings(strings) => {
-            for (index, string) in strings.iter().enumerate() {
-              ui.horizontal(|ui| {
-                let button = egui::Button::new(string);
-                if ui.add_sized(ui.available_size(), button).clicked() {
-                  selection = Some(Response::Index(index));
-                }
-              });
-            }
+        ui.vertical_centered(|ui| {
+          for (index, text) in choices.into_iter().enumerate() {
+            ui.horizontal(|ui| {
+              let button = egui::Button::new(text);
+              if ui.add_sized(ui.available_size(), button).clicked() {
+                selection = Some(Response::Index(index));
+              }
+            });
           }
         });
-
-        ui.separator();
-
-        if ui.button("Cancel").clicked() {
-          selection = Some(Response::Cancel);
-        }
       });
 
     if !open || ctx.input().key_pressed(egui::Key::Escape) {
@@ -53,11 +33,6 @@ impl SelectDlg {
 
     selection
   }
-}
-
-pub enum Choices<'a> {
-  Paths(&'a Vec<PathBuf>),
-  Strings(&'a Vec<String>),
 }
 
 pub enum Response {
