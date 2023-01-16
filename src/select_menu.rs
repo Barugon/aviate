f76aa243dd7dec04e34470ca47c75f778307caa9
Barugon <16503728+Barugon@users.noreply.github.com>
2023-01-16 03:37:22 -0800

    Remove size code that's no longer necessary

--- src/select_menu.rs
+++ src/select_menu.rs
@@ -2,17 +2,17 @@ use eframe::{egui, emath};
 
 #[derive(Default)]
 pub struct SelectMenu {
-  metrics: Option<(emath::Pos2, f32)>,
+  pos: Option<emath::Pos2>,
 }
 
 impl SelectMenu {
   pub fn set_pos(&mut self, pos: emath::Pos2) {
-    self.metrics = Some((pos, 0.0));
+    self.pos = Some(pos);
   }
 
   pub fn show(&mut self, ctx: &egui::Context, choices: &[String]) -> Option<Response> {
     let mut selection = None;
-    if let Some((pos, width)) = &mut self.metrics {
+    if let Some(pos) = &mut self.pos {
       let response = egui::Area::new("choices")
         .order(egui::Order::Foreground)
         .fixed_pos(*pos)
@@ -20,14 +20,9 @@ impl SelectMenu {
           egui::Frame::popup(ui.style()).show(ui, |ui| {
             for (index, choice) in choices.iter().enumerate() {
               ui.horizontal(|ui| {
-                let size = emath::Vec2::new(*width, ui.available_height());
-                let layout = egui::Layout::centered_and_justified(egui::Direction::LeftToRight)
-                  .with_main_align(emath::Align::Min);
-                ui.allocate_ui_with_layout(size, layout, |ui| {
-                  if ui.selectable_label(false, choice).clicked() {
-                    selection = Some(Response::Index(index));
-                  }
-                });
+                if ui.selectable_label(false, choice).clicked() {
+                  selection = Some(Response::Index(index));
+                }
               });
             }
           });
@@ -38,12 +33,6 @@ impl SelectMenu {
       if response.clicked_elsewhere() {
         selection = Some(Response::Close);
       } else {
-        let style = ctx.style();
-        let margin = style.spacing.window_margin.left + style.spacing.window_margin.right;
-
-        // Grab the interior width.
-        *width = (response.rect.width() - margin).max(0.0);
-
         // Make sure that the popup doesn't go past the window's edges.
         let available = ctx.available_rect();
 
@@ -64,7 +53,7 @@ impl SelectMenu {
     }
 
     if selection.is_some() {
-      self.metrics = None;
+      self.pos = None;
     }
 
     selection
