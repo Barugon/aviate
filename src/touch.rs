a124af3a0797612ca20098755286a423f83b8cec
Barugon <16503728+Barugon@users.noreply.github.com>
2023-05-11 13:39:07 -0700

    Better `LongPressTracker` logic

--- src/touch.rs
+++ src/touch.rs
@@ -1,12 +1,12 @@
-use std::{time, sync::mpsc, thread, collections};
-use eframe::{epaint, egui};
 use crate::util;
+use eframe::{egui, epaint};
+use std::{collections, sync::mpsc, thread, time};
 
-const LONG_PRESS_SECS: f64 = 1.0;
+const LONG_PRESS_DUR: time::Duration = time::Duration::from_secs(1);
 
 enum Request {
   Refresh,
-  Clear,
+  Cancel,
   Exit,
 }
 
@@ -30,27 +30,30 @@ impl LongPressTracker {
       thread::Builder::new()
         .name("app::TouchTracker thread".to_owned())
         .spawn(move || loop {
-          let mut request = receiver.recv().expect(util::FAIL_ERR);
-          let mut refresh;
+          let mut request = Some(receiver.recv().expect(util::FAIL_ERR));
+          let mut time = None;
           loop {
-            match request {
-              Request::Refresh => refresh = true,
-              Request::Clear => refresh = false,
-              Request::Exit => return,
+            if let Some(request) = request.take() {
+              match request {
+                Request::Refresh => time = Some(time::SystemTime::now()),
+                Request::Cancel => time = None,
+                Request::Exit => return,
+              }
             }
 
             // Check for another request.
-            match receiver.try_recv() {
-              Ok(rqst) => request = rqst,
-              Err(_) => break,
+            if let Ok(rqst) = receiver.try_recv() {
+              request = Some(rqst);
             }
-          }
 
-          // When Refresh is sent, we wait for the required time and then send a refresh request.
-          // This will allow the main thread to wake up and update TrackerTimer.
-          if refresh {
-            thread::sleep(time::Duration::from_secs_f64(LONG_PRESS_SECS));
-            ctx.request_repaint();
+            if check_time(time) {
+              ctx.request_repaint();
+              time = None;
+            }
+
+            if request.is_none() && time.is_none() {
+              break;
+            }
           }
         })
         .expect(util::FAIL_ERR),
@@ -68,34 +71,42 @@ impl LongPressTracker {
   pub fn set(&mut self, id: egui::TouchId, phase: egui::TouchPhase, pos: epaint::Pos2) {
     match phase {
       egui::TouchPhase::Start => {
+        // Only allow one touch.
         if self.ids.is_empty() {
           let time = time::SystemTime::now();
           self.info = Some(TouchInfo { time, pos });
-          self.ids.insert(id.0);
           self.sender.send(Request::Refresh).expect(util::FAIL_ERR);
         } else {
-          self.info = None;
-          self.sender.send(Request::Clear).expect(util::FAIL_ERR);
+          self.remove_info();
         }
+        self.ids.insert(id.0);
+      }
+      egui::TouchPhase::Move => {
+        self.remove_info();
       }
       _ => {
+        // For End and Cancel, we remove the ID.
         self.ids.remove(&id.0);
-        self.info = None;
-        self.sender.send(Request::Clear).expect(util::FAIL_ERR);
+        self.remove_info();
       }
     }
   }
 
   pub fn update(&mut self) {
     if let Some(info) = self.info.take() {
-      if let Ok(dur) = time::SystemTime::now().duration_since(info.time) {
-        if dur.as_secs_f64() > LONG_PRESS_SECS {
+      if let Ok(duration) = time::SystemTime::now().duration_since(info.time) {
+        if duration >= LONG_PRESS_DUR {
           self.pos = Some(info.pos);
           return;
         }
         self.info = Some(info);
       }
     }
+  }
+
+  fn remove_info(&mut self) {
+    self.info = None;
+    self.sender.send(Request::Cancel).expect(util::FAIL_ERR);
   }
 }
 
@@ -108,4 +119,15 @@ impl Drop for LongPressTracker {
       thread.join().expect(util::FAIL_ERR);
     }
   }
+}
+
+fn check_time(time: Option<time::SystemTime>) -> bool {
+  if let Some(time) = time {
+    if let Ok(duration) = time::SystemTime::now().duration_since(time) {
+      if duration >= LONG_PRESS_DUR {
+        return true;
+      }
+    }
+  }
+  false
 }
