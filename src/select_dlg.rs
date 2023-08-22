e7f21eb18def12d6ecd83f8884015037fb8adf07
Barugon <16503728+Barugon@users.noreply.github.com>
2023-08-22 01:51:21 -0700

    Implement hack to reset size of `SelectDlg`

--- src/select_dlg.rs
+++ src/select_dlg.rs
@@ -1,7 +1,11 @@
+use std::mem;
+
 use eframe::{egui, emath};
 
 #[derive(Default)]
-pub struct SelectDlg;
+pub struct SelectDlg {
+  reset: bool,
+}
 
 impl SelectDlg {
   pub fn show<'a, I: Iterator<Item = &'a str>>(
@@ -11,32 +15,40 @@ impl SelectDlg {
   ) -> Option<Response> {
     let mut selection = None;
     let mut open = true;
-    egui::Window::new(egui::RichText::from("👉  Select").strong())
+    let win = egui::Window::new(egui::RichText::from("👉  Select").strong())
       .open(&mut open)
       .collapsible(false)
       .resizable(false)
-      .anchor(emath::Align2::CENTER_CENTER, [0.0, 0.0])
-      .default_width(200.0)
-      .show(ctx, |ui| {
-        ui.add_space(8.0);
-        ui.vertical_centered(|ui| {
-          egui::ScrollArea::vertical().show(ui, |ui| {
-            for (index, text) in choices.enumerate() {
-              ui.horizontal(|ui| {
-                let widget = egui::SelectableLabel::new(false, text);
-                if ui.add_sized(ui.available_size(), widget).clicked() {
-                  selection = Some(Response::Index(index));
-                }
-              });
-            }
-          });
+      .anchor(emath::Align2::CENTER_CENTER, [0.0, 0.0]);
+
+    // Hack to reset the window size.
+    let win = if mem::take(&mut self.reset) {
+      win.fixed_size([200.0, 500.0])
+    } else {
+      win
+    };
+
+    win.show(ctx, |ui| {
+      ui.add_space(8.0);
+      ui.vertical_centered(|ui| {
+        egui::ScrollArea::vertical().show(ui, |ui| {
+          for (index, text) in choices.enumerate() {
+            ui.horizontal(|ui| {
+              let widget = egui::SelectableLabel::new(false, text);
+              if ui.add_sized(ui.available_size(), widget).clicked() {
+                selection = Some(Response::Index(index));
+              }
+            });
+          }
         });
       });
+    });
 
     if !open || ctx.input(|state| state.key_pressed(egui::Key::Escape)) {
       selection = Some(Response::Close);
     }
 
+    self.reset = selection.is_some();
     selection
   }
 }
