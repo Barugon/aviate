75b79936b8ceebef2d0d9e84906e804712905e33
Barugon <16503728+Barugon@users.noreply.github.com>
2024-12-03 17:51:49 -0800

    Use Godot's `ConfigFile` for storage

--- src/config.rs
+++ src/config.rs
@@ -1,156 +1,96 @@
 use crate::{geom, util};
-use godot::{classes::Json, prelude::*};
-use std::{cell, path, rc, sync::atomic};
+use godot::{
+  classes::{ConfigFile, Json},
+  prelude::*,
+};
+use std::sync;
 
 /// Storage for configuration items, persisted as JSON.
 #[derive(Clone)]
 pub struct Storage {
-  items: rc::Rc<cell::RefCell<Items>>,
+  items: sync::Arc<sync::RwLock<Gd<ConfigFile>>>,
 }
 
 impl Storage {
   pub fn new() -> Self {
-    let items = rc::Rc::new(cell::RefCell::new(Items::load(Storage::path())));
+    let mut items = ConfigFile::new_gd();
+    items.load(&Storage::path());
+
+    let items = sync::Arc::new(sync::RwLock::new(items));
     Self { items }
   }
 
   pub fn set_win_info(&mut self, win_info: &util::WinInfo) {
     let value = win_info.to_variant();
-    let mut items = (*self.items).borrow_mut();
-    items.set(Storage::WIN_INFO_KEY, value);
-    items.store();
+    self.set_value(Storage::WIN_INFO_KEY, &value);
   }
 
   #[allow(unused)]
   pub fn get_win_info(&self) -> util::WinInfo {
-    let items = (*self.items).borrow();
-    util::WinInfo::from_variant(items.get(Storage::WIN_INFO_KEY))
+    util::WinInfo::from_variant(self.get_value(Storage::WIN_INFO_KEY))
   }
 
   pub fn set_night_mode(&mut self, dark: bool) {
     let value = Variant::from(dark);
-    let mut items = (*self.items).borrow_mut();
-    items.set(Storage::NIGHT_MODE_KEY, value);
-    items.store();
+    self.set_value(Storage::NIGHT_MODE_KEY, &value);
   }
 
   pub fn get_night_mode(&self) -> Option<bool> {
-    let items = (*self.items).borrow();
-    match items.get(Storage::NIGHT_MODE_KEY)?.try_to::<bool>() {
-      Ok(value) => return Some(value),
-      Err(err) => godot_error!("{err}"),
-    }
-    None
+    let val = self.get_value(Storage::NIGHT_MODE_KEY)?;
+    val.try_to::<bool>().ok()
   }
 
   pub fn set_show_bounds(&mut self, bounds: bool) {
     let value = Variant::from(bounds);
-    let mut items = (*self.items).borrow_mut();
-    items.set(Storage::SHOW_BOUNDS_KEY, value);
-    items.store();
+    self.set_value(Storage::SHOW_BOUNDS_KEY, &value);
   }
 
   pub fn get_show_bounds(&self) -> Option<bool> {
-    let items = (*self.items).borrow();
-    match items.get(Storage::SHOW_BOUNDS_KEY)?.try_to::<bool>() {
-      Ok(value) => return Some(value),
-      Err(err) => godot_error!("{err}"),
-    }
-    None
+    let val = self.get_value(Storage::SHOW_BOUNDS_KEY)?;
+    val.try_to::<bool>().ok()
   }
 
   pub fn set_asset_folder(&mut self, folder: GString) {
     let value = Variant::from(folder);
-    let mut items = (*self.items).borrow_mut();
-    items.set(Storage::ASSET_FOLDER_KEY, value);
-    items.store();
+    self.set_value(Storage::ASSET_FOLDER_KEY, &value);
   }
 
   pub fn get_asset_folder(&self) -> Option<GString> {
-    let items = (*self.items).borrow();
-    match items.get(Storage::ASSET_FOLDER_KEY)?.try_to::<GString>() {
-      Ok(value) => return Some(value),
-      Err(err) => godot_error!("{err}"),
+    let val = self.get_value(Storage::ASSET_FOLDER_KEY)?;
+    val.try_to::<GString>().ok()
+  }
+
+  fn set_value(&mut self, key: &str, val: &Variant) {
+    if let Ok(mut items) = self.items.write() {
+      items.set_value(Storage::CONFIG_SECTION, key, val);
+    }
+  }
+
+  fn get_value(&self, key: &str) -> Option<Variant> {
+    if let Ok(items) = self.items.read() {
+      if items.has_section_key(Storage::CONFIG_SECTION, key) {
+        return Some(items.get_value(Storage::CONFIG_SECTION, key));
+      }
     }
     None
   }
 
-  fn path() -> path::PathBuf {
-    let folder = util::get_config_folder().to_string();
-    let folder = path::PathBuf::from(folder);
-    folder.join(util::APP_NAME).with_extension("json")
+  fn path() -> String {
+    format!("user://{}.cfg", util::APP_NAME)
   }
 
+  const CONFIG_SECTION: &'static str = "config";
   const WIN_INFO_KEY: &'static str = "win_info";
   const NIGHT_MODE_KEY: &'static str = "night_mode";
   const SHOW_BOUNDS_KEY: &'static str = "show_bounds";
   const ASSET_FOLDER_KEY: &'static str = "asset_folder";
 }
 
-struct Items {
-  path: path::PathBuf,
-  items: Dictionary,
-  changed: atomic::AtomicBool,
-}
-
-impl Items {
-  fn load(path: path::PathBuf) -> Self {
-    #[cfg(feature = "dev")]
-    convert_bounds_svgs();
-
-    let items = Self::load_items(&path);
-    let changed = atomic::AtomicBool::new(false);
-    Self {
-      path,
-      items,
-      changed,
-    }
-  }
-
-  fn get(&self, key: &str) -> Option<Variant> {
-    self.items.get(key)
-  }
-
-  fn set(&mut self, key: &str, item: Variant) {
-    let existing = self.items.get_or_nil(key);
-    if item.try_to::<Dictionary>().is_ok() {
-      if Json::stringify(&existing) == Json::stringify(&item) {
-        return;
-      }
-    } else if existing == item {
-      return;
-    }
-
-    self.items.set(key, item);
-    self.changed.store(true, atomic::Ordering::Relaxed);
-  }
-
-  fn store(&self) {
-    if self.changed.swap(false, atomic::Ordering::Relaxed) {
-      let text = Json::stringify(&Variant::from(self.items.clone()));
-      util::store_text(&self.path, &text);
-    }
-  }
-
-  fn load_items(path: &path::Path) -> Dictionary {
-    if let Some(text) = util::load_text(path) {
-      let items = Json::parse_string(&text);
-      match items.try_to::<Dictionary>() {
-        Ok(items) => {
-          return items;
-        }
-        Err(err) => {
-          godot_error!("{:?}: {}", path, err);
-        }
-      }
-    }
-    Dictionary::new()
-  }
-}
-
-impl Drop for Items {
+impl Drop for Storage {
   fn drop(&mut self) {
-    self.store();
+    if let Ok(mut items) = self.items.write() {
+      items.save(&Storage::path());
+    }
   }
 }
 
