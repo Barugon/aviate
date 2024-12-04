0792bda8e2d47dbb610dc4b775f9fe1e938dbb78
Barugon <16503728+Barugon@users.noreply.github.com>
2024-12-03 16:28:51 -0800

    Remove unnecessary code

--- build.rs
+++ build.rs
@@ -1,7 +1,5 @@
-use std::path::Path;
-
 fn main() {
-  // This script is only needed for static builds.
+  // This script is only needed for GDAL static builds.
   let Some(build_static) = option_env!("GDAL_STATIC") else {
     return;
   };
@@ -10,17 +8,7 @@ fn main() {
     return;
   }
 
-  // Get the GDAL_HOME path.
-  #[allow(clippy::option_env_unwrap)]
-  let gdal_home = option_env!("GDAL_HOME").expect("GDAL_HOME not set");
-  assert!(!gdal_home.is_empty());
-
-  // Set the library search path.
-  let lib_path = Path::new(gdal_home).join("lib");
-  assert!(lib_path.is_dir());
-  println!("cargo::rustc-link-search={lib_path:?}");
-
-  // Libraries to link against.
+  // GDAL dependencies.
   let libs = [
     "crypto",
     "curl",
